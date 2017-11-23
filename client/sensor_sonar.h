3c7b5c1f32c0adbfe48c85f671a8755375721170
spierre37 <samuel.pierre37@yahoo.fr>
2017-11-23 15:35:13 +0100

    Rewriting Sonar sensor functions

---
+++ client/sensor_sonar.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ev3.h"
+#include "ev3_port.h"
+#include "ev3_sensor.h"
+// WIN32 /////////////////////////////////////////
+#ifdef __WIN32__
+
+#include <windows.h>
+
+// UNIX //////////////////////////////////////////
+#else
+
+#include <unistd.h>
+#define Sleep( msec ) usleep(( msec ) * 1000 )
+
+//////////////////////////////////////////////////
+#endif
+
+void initSensorSonar();
+/*need to be started at the beginning
+ Allows to use the sonar sensor */
+int get_sonar_distance();
+/* return the distance found by the sensor in mm.
+*/
