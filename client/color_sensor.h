551d7ba9a5f1c027242457422451b4b69720511c
Benjamin Castellan <benjamincastellan@gmail.com>
2017-11-24 13:44:26 +0100

    add of file detecting the color

---
+++ client/color_sensor.h
@@ -0,0 +1,10 @@
+#ifndef R0B0T_MAKING_COLOR_SENSOR_H
+#define R0B0T_MAKING_COLOR_SENSOR_H
+
+#endif //R0B0T_MAKING_COLOR_SENSOR_H
+
+int getColor();
+//return an array with
+
+int red_obstacle();
+//say if an obstacle
