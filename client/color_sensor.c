3c25719271134d3462d47e5844d3532562d04234
lroman <louis.roman@telecom-paristech.fr>
2017-12-07 23:01:19 +0100

    Récupération des 3 valeurs du capteur de couleur : r,g,b.

--- client/color_sensor.c
+++ client/color_sensor.c
@@ -23,7 +23,9 @@
 const char const *color[] = { "?", "BLACK", "BLUE", "GREEN", "YELLOW", "RED", "WHITE", "BROWN" };
 #define COLOR_COUNT  (( int )( sizeof( color ) / sizeof( color[ 0 ])))
 
-int value[3];
+int value_r;
+int value_g;
+int value_b;
 
 void getColor(){
 
@@ -32,17 +34,19 @@ void getColor(){
     if ( ev3_search_sensor( LEGO_EV3_COLOR, &sn_color, 0 )) {
         printf( "COLOR sensor is found, reading COLOR...\n" );
         set_sensor_mode_inx(sn_color,LEGO_EV3_COLOR_RGB_RAW);
-        get_sensor_value( 0, sn_color, value );
+        get_sensor_value( 0, sn_color, &value_r );
+		get_sensor_value( 1, sn_color, &value_g);
+		get_sensor_value( 2, sn_color, &value_b);
         fflush( stdout );
     }
 }
 
 int red_obstacle(){
     getColor();
-    if (value[0] > 0) printf("obstacle red found\n");
-    else printf("obstacle red\n");
-    printf("returned values are %d %d %d\n",value[0],value[1],value[2]);
-    return (value[0] > 0);
+    if ((value_r > 10) && (value_g < 10) && (value_b < 10)) printf("obstacle red found\n");	/*tests a modifier, mais l'idee est là*/
+    else printf("not red\n");
+	printf("returned values are %d %d %d\n",value_r,value_g,value_b);
+	return (value_r > 0);
 }
 
 int main( void )
