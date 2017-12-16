59adef71e989c387775988a506a9ce9e8cbc8a3f
spierre37 <samuel.pierre37@yahoo.fr>
2017-12-16 18:52:04 +0100

    Résolution erreur test 2

--- client/test_final_2.c
+++ client/test_final_2.c
@@ -5,6 +5,10 @@
 #include "mvt_forward.h"
 #include "mvt_rotate.h"
 
+#ifndef PI
+#define PI 3.14159265
+#endif
+
 char main (void) {
 /*
  * The robot go straight and stop when he detects a wall to close ( < 50 mm )
@@ -25,7 +29,7 @@ char main (void) {
 	scanf("%d",&initialY);
 	printf("\n");
 	printf("Current TETA : \n");
-	scanf("%d",&initialY);
+	scanf("%d",&initialTETA);
 	printf("\n");
 	
 	int finalX = 0;
@@ -37,23 +41,23 @@ char main (void) {
 	
 	int firstRotation;
 	if(deltaX>0 && deltaY>0){
-		firstRotation = (int) 90-(atan(fabs(deltaY/deltaX))*360)/PI;
+		firstRotation = (int) 90-(atan(fabs(deltaY/deltaX))*180)/PI;
 	}
 	else if(deltaX>0 && deltaY<0){
-		firstRotation = (int) 90+(atan(fabs(deltaY/deltaX))*360)/PI;
+		firstRotation = (int) 90+(atan(fabs(deltaY/deltaX))*180)/PI;
 	}
 	else if(deltaX<0 && deltaY>0){
-		firstRotation = (int) -(90-(atan(fabs(deltaY/deltaX))*360)/PI);
+		firstRotation = (int) -(90-(atan(fabs(deltaY/deltaX))*180)/PI);
 	}
 	else{
-		firstRotation = (int) -(90+(atan(fabs(deltaY/deltaX))*360)/PI);
+		firstRotation = (int) -(90+(atan(fabs(deltaY/deltaX))*180)/PI);
 	}
 	int speed = getTachoMaxSpeed()/5;
 	int securityDistance = 150;
 	int amplitudeSweep = 50;
 	
 	rotate_to_angle(speed,firstRotation-initialTETA);
-	int distance = (int) sqrt(pow(deltaX,2)+pow(deltaY,2)):
+	int distance = (int) sqrt(pow(deltaX,2)+pow(deltaY,2));
 	go_to_distance_sweep(speed, distance, securityDistance, amplitudeSweep);
 	rotate_to_angle(speed,finalTETA-TETA);
 	
